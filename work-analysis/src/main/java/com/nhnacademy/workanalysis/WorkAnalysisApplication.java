810896e9dd1ce79f133a4191c2d2249999ac29c7
Migong0311 <kimms000311@gmail.com>
2025-05-13 15:13:25 +0900

    chore : first commit

---
+++ work-analysis/src/main/java/com/nhnacademy/workanalysis/WorkAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.workanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkAnalysisApplication.class, args);
+	}
+
+}
