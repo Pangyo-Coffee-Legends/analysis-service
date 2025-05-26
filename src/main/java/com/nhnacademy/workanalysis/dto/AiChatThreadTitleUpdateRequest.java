c6dc3a144f28704a1e4dc44625166bb6613c99b7
Migong0311 <kimms000311@gmail.com>
2025-05-26 14:04:43 +0900

    feat : 대화 목록 제목 수정 완료 fixes #1

--- src/main/java/com/nhnacademy/workanalysis/dto/AiChatThreadTitleUpdateRequest.java
+++ src/main/java/com/nhnacademy/workanalysis/dto/AiChatThreadTitleUpdateRequest.java
@@ -1,18 +1,12 @@
 package com.nhnacademy.workanalysis.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 public class AiChatThreadTitleUpdateRequest {
-
-    @NotNull
-    private Long threadId;
-    @NotBlank
+    @NotBlank(message = "제목은 필수입니다.")
     private String title;
-
-
 }
