d7dded584bb28a061bddceeab719e59778fc1523
Migong0311 <kimms000311@gmail.com>
2025-05-27 11:17:45 +0900

    refactor : 반영된 PR 리뷰 사항 수정 fixes #1

--- src/main/java/com/nhnacademy/workanalysis/dto/AiChatThreadTitleUpdateRequest.java
+++ src/main/java/com/nhnacademy/workanalysis/dto/AiChatThreadTitleUpdateRequest.java
@@ -4,6 +4,9 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * 대화목록(이하 '쓰레드') 제목 수정시 필요한 DTO입니다.
+ */
 @Data
 @NoArgsConstructor
 public class AiChatThreadTitleUpdateRequest {
